3a5ae78dc2e56486d919d6a75509fa648376310f
m-j-terry <michaeljterry0815@gmail.com>
2023-05-02 22:48:42 -0400

    frogger movement/keyboard commands set

--- node.js
+++ node.js
@@ -9,7 +9,14 @@ const colors = {
 /*----- state variables -----*/
 
 let board
-let frogger = -1
+let innerArr = null
+let frogger = {
+    number : -1,
+    row : 10,
+    column : 6,
+    previousColor : 3,
+    life : -1
+}
 
 /*----- cached elements  -----*/
 
@@ -17,16 +24,20 @@ let frogger = -1
 
 
 /*----- event listeners -----*/
-// addEventListener('keypress', => {
-//  if ('keypress' = 'ArrowUp' || 'KeyW') {
-//        ...
-//    } else if ('keypress' = 'ArrowDown' || 'KeyS') {
-//        ...
-//    } else if ('keypress' = 'ArrowLeft' || 'KeyA') {
-//        ...
-//    } else ('keypress' = 'ArrowRight' || 'KeyD') {
-//        ...
-//    }
+addEventListener('keydown', (event) => {
+    if (event.code == 'ArrowUp' || event.code == 'KeyW') {
+        hopForward()
+    } else if (event.code == 'ArrowDown' || event.code == 'KeyS') {
+        hopBackward()
+    } else if (event.code == 'ArrowLeft' || event.code == 'KeyA') {
+        hopLeft()
+    } else if (event.code == 'ArrowRight' || event.code == 'KeyD') {
+        hopRight()
+    }
+})
+
+// addEventListener('click', (e) => {
+//     riverFlow()
 // })
 
 // addEventListener('click', riverFlow)
@@ -51,42 +62,119 @@ function init() {
     render()
 }
 
-function riverFlow() {
-
-        // for (let i = 0; i < river6.length; i++){
-        //     river6[i] = river6.indexOf(i + 1)
-        // }
-
-}
-
 function render() {
     renderBoard()
 }
 
 function renderBoard() {
-let innerArr = null
-for (let i = 0; i < board.length; i++) {
-    innerArr = board[i]
-    generateRow()
-    function generateRow() {
-        for (let i = 0; i < innerArr.length; i++) {
-        const cellEl = document.getElementById("c" + i + "r" +board.indexOf(innerArr))
-        cellEl.style.backgroundColor = colors[innerArr[i]]
+    for (let i = 0; i < board.length; i++) {
+        innerArr = board[i]
+        generateRow()
+        function generateRow() {
+            for (let i = 0; i < innerArr.length; i++) {
+            const cellEl = document.getElementById("c" + i + "r" +board.indexOf(innerArr))
+            cellEl.style.backgroundColor = colors[innerArr[i]]
+            }
+        }
     }
-}
-}
-}
-
-
-function frogHop() {
-    if (frogger === -1) {
-        render()
-    } else {
+    if (frogger.life === 0) {
         gameOver()
     }
 }
 
+
+/* Hopping functions */
+let frogHop = board[frogger.row]
+
+function hopForward() {
+    console.log('hop up')
+    //change frogger square color to preset value 
+    frogHop.splice(frogger.column -1, 1, frogger.previousColor)
+    frogger.row = frogger.row -1
+    //record the color of the square that frogger will jump to and see if Frogger fell in the water or not!
+    frogHop = board[frogger.row]
+    frogger.previousColor = frogHop[frogger.column -1]
+    frogger.life = frogger.life * frogHop[frogger.column -1]
+    //change the color of the space frogger jumps to.
+    frogHop.splice(frogger.column -1, 1, frogger.number)
+    renderBoard()
+}
+
+function hopLeft() {
+    console.log('hop left')
+    //change frogger square color to preset value
+    frogHop.splice(frogger.column -1, 1, frogger.previousColor)
+    frogger.column = frogger.column - 1
+    //record the color of the square that frogger will jump to and see if Frogger fell in the water or not!
+    frogHop = board[frogger.row]
+    frogger.previousColor = frogHop[frogger.column -1]
+    frogger.life = frogger.life * frogHop[frogger.column -1]
+    //change the color of the space frogger jumps to.
+    frogHop.splice(frogger.column -1, 1, frogger.number)
+    renderBoard()
+}
+
+
+function hopRight() {
+    console.log('hop right')
+    //change frogger square color to preset value
+    frogHop.splice(frogger.column -1, 1, frogger.previousColor)
+    frogger.column = frogger.column + 1
+    //record the color of the square that frogger will jump to and see if Frogger fell in the water or not!
+    frogHop = board[frogger.row]
+    frogger.previousColor = frogHop[frogger.column -1]
+    frogger.life = frogger.life * frogHop[frogger.column -1]
+    //change the color of the space frogger jumps to.
+    frogHop.splice(frogger.column -1, 1, frogger.number)
+    renderBoard()
+}
+
+function hopBackward() {
+    console.log('hop back')
+    //change frogger square color to preset value
+    frogHop.splice(frogger.column -1, 1, frogger.previousColor)
+    frogger.row = frogger.row + 1
+    //record the color of the square that frogger will jump to and see if Frogger fell in the water or not!
+    frogHop = board[frogger.row]
+    frogger.previousColor = frogHop[frogger.column -1]
+    frogger.life = frogger.life * frogHop[frogger.column -1]
+    //change the color of the space frogger jumps to.
+    frogHop.splice(frogger.column -1, 1, frogger.number)
+    renderBoard()
+}
+
+
+
+function riverFlow() {
+    let river6 = board[1]
+        river6.push(river6[0])
+        river6.shift()
+    let river5 = board[2]
+        river5.unshift(river5[9])
+        river5.pop()
+    let river4 = board[3]
+        river4.push(river4[0])
+        river4.shift()
+    let river3 = board[6]
+        river3.unshift(river3[9])
+        river3.pop()
+    let river2 = board[7] //needs a new pattern to sync up with river 3
+        river2.push(river2[0])
+        river2.shift()
+    let river1 = board[9]
+        river1.unshift(river1[9])
+        river1.pop()
+    render()
+    }
+
+// }
+
+// const riverInterval = setInterval(riverFlow, 1500)
+//Perhaps instead of pushing and popping/ shifting and unshifting,and manipulating the preexisting elements of the array, which causes interference in the patterns, you could nest several different intervals within a closure that send the different patterns at different times. 
+
+
 function gameOver() {
-    // clearInterval(riverInterval)
+    clearInterval(riverInterval)
+    alert('Oh no, Frogger got swept away by the river!')
     // use a modal/carousel/.addClass to toggle pages and buttons to become visible.
 }
